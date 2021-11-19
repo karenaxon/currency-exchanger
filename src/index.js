b0312869e283f9c407419105e30060b3a912e07c
Kaxon <karenaxon@users.noreply.github.com>
2021-11-19 14:01:11 -0800

    Add code to pcreate and populate dropdown menus.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from './js/exchange-service.js';
+import InitialCallService from './js/initial-call-service.js';
 
 function getElements(response) {
   if (response) {
     $('#display-exchange').text(`The exchange is ${response.conversion_result}%`);
-  } else {
-    $('.showErrors').text(`There was an error: ${response}`);
-  }
-}
-function createMenus(response) {
-  if (response) {
-    $('#baseCode').append(`<option value="${response.conversion_rates}> ${response.conversion_rates} </option>`);
   } else {
     $('.showErrors').text(`There was an error: ${response}`);
   }
@@ -24,10 +18,34 @@ async function makeApiCall(fromCode, toCode, number) {
   getElements(response);
 }
 
+function createDropdownMenus(data) {
+  if (data) {
+    const conversionRates = data.conversion_rates;
+    
+    for (const key in conversionRates) {
+      console.log(key);
+      $('#baseCode').append(`<option value="${key}"> ${key} </option>`);
+      $('#targetCode').append(`<option value="${key}"> ${key} </option>`);
+    } 
+    
+  } else {
+    $('.showErrors').text(`There was an error: ${data}`);
+  }
+}
+
+async function initialCall() {
+  const response = await InitialCallService.getInitialResponse();
+  createDropdownMenus(response);
+}
+
+initialCall();
+
 $(document).ready(function() {
   $('#btn').click(function() {
-    let inputAmount = parseFloat($('#input').val());
+    const inputAmount = parseFloat($('#input').val());
+    const fromCurrencyCode = $('#baseCode').val();
+    const toCurrencyCode = $('#targetCode').val();
     // clearFields();
-    makeApiCall("USD", "EUR", inputAmount);
+    makeApiCall(fromCurrencyCode, toCurrencyCode, inputAmount);
   });
 });
